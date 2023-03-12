6903ae9a4e167c1db776818ea645f46c428d35fa
Thomas Chimbault <thomas.chimbault@icloud.com>
2023-03-12 23:54:04 +0100

    first version scraper available

---
+++ code-scripts/scraping-trends.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://trendlistz.com/united-states'
+response = requests.get(url)
+content = response.content
+
+soup = BeautifulSoup(content, 'html.parser')
+
+trends_container = soup.find('ol', class_='trend-list card')
+
+trends = []
+for trend in trends_container.find_all('span'):
+    trends.append(trend.text.strip())
+print(trends)
