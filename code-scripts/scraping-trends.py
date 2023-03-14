d3eef3a33b996563353ff63f922886ee5e46fbe6
Thomas Chimbault <thomas.chimbault@icloud.com>
2023-03-14 17:47:43 +0100

    scraping twitter trends live

--- code-scripts/scraping-trends.py
+++ code-scripts/scraping-trends.py
@@ -10,6 +10,6 @@ soup = BeautifulSoup(content, 'html.parser')
 trends_container = soup.find('ol', class_='trend-list card')
 
 trends = []
-for trend in trends_container.find_all('span'):
+for trend in trends_container.find_all('span', class_="term"):
     trends.append(trend.text.strip())
 print(trends)
