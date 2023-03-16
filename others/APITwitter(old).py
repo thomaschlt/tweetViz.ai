d9ea7dc3e4640251538daafb9d0d095f9c0de387
Thomas Chimbault <thomas.chimbault@icloud.com>
2023-03-16 22:07:22 +0100

    modif api access

--- others/APITwitter(old).py
+++ others/APITwitter(old).py
@@ -1,26 +1,19 @@
 import tweepy as tw
 
 # Replace with your own API credentials
-consumer_key = 'ghMquretmt48Zxvhh7LDtmjuE'
-consumer_secret = 'nSwTBYzOAKFQFCUjIqsF5ahgnlzU1HyGwCe4Z7pyOUEZ0uf0mv'
-access_token = '797893716023373824-xiURxyBYevxxJEA6G2ZwUpwrk3O3LK0'
+key = 'ghMquretmt48Zxvhh7LDtmjuE'
+secret_consumer = 'nSwTBYzOAKFQFCUjIqsF5ahgnlzU1HyGwCe4Z7pyOUEZ0uf0mv'
+token = '797893716023373824-xiURxyBYevxxJEA6G2ZwUpwrk3O3LK0'
 access_token_secret = 'pN39KYOdTfCIqNZiwWPEFQOZDZalPh0SB2IzRxIJiWEpT'
 
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tw.OAuthHandler(key, secret_consumer)
+auth.set_access_token(token, access_token_secret)
 
 api = tw.API(auth)
-
 user_id = 'USER_ID'
-
 followers = []
 for follower in tw.Cursor(api.followers_ids, id=user_id).items():
     followers.append(follower)
 
 for follower in followers:
     print(follower)
-
-# Access
-# 797893716023373824-xiURxyBYevxxJEA6G2ZwUpwrk3O3LK0 access token
-# access token secret : pN39KYOdTfCIqNZiwWPEFQOZDZalPh0SB2IzRxIJiWEpT
-# Bearer Token : AAAAAAAAAAAAAAAAAAAAAGZ3lwEAAAAAYjNLY1jc3RHgBddPqlQd%2Buj0fVY%3DtTO0XId13Yd6sPNzJQa7tGuUlhrXPYEbYlCo3CS6iH6jnaLWrj
