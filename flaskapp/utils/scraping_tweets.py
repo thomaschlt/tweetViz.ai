0f16b33da045faef232345d14f2488a87d9a28b1
Thomas Chimbault <thomas.chimbault@icloud.com>
2023-04-21 17:26:32 +0200

    new projet restructuring

--- flaskapp/utils/scraping_tweets.py
+++ flaskapp/utils/scraping_tweets.py
@@ -12,14 +12,29 @@ from sentiment_analysis import predict_tweet
 
 
 def scrape_tweet(trend: str):
+    neg = []
+    neu = []
+    pos = []
     query = trend + " since:2023-04-16"
     data = pd.DataFrame(itertools.islice(
         sntwitter.TwitterSearchScraper(query).get_items(), 500))  # None ?
     data = data[data.apply(is_english, axis=1)]
     df = data[['id', 'date', 'rawContent', 'user', 'replyCount',
                'retweetCount', 'likeCount', 'quoteCount']]
+    for k in range(0, len(df)):
+        tweet = df["rawContent"].iloc[k]
+        scores = predict_tweet(tweet)
+        neg.append(scores[0])
+        neu.append(scores[1])
+        pos.append(scores[2])
+    df["Roberta_neg"] = neg
+    df["Roberta_neu"] = neu
+    df["Roberta_pos"] = pos
     df.to_csv("static/data/" + trend + ".csv", index=False)
 
 
 def is_english(tweet: str):
     return tweet.lang == 'en'
+
+
+scrape_tweet("Maguire")
