f5abef0a75946d0650405251c866072f018beeb1
Thomas Chimbault <thomas.chimbault@icloud.com>
2023-04-21 17:31:08 +0200

    delete old work

--- old-works/code-scripts/main.py
+++
@@ -1,9 +0,0 @@
-from trendScraper import scrape_trend
-from csv import reader
-
-with open('trends.csv', 'r') as f:
-    r = reader(f)
-    header = next(r)
-    if header != None:
-        for row in r:
-            scrape_trend(row[0])
